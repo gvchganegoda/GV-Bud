7c40f355504725f9fa28b196bc3b1c456710bd16
gvchganegoda <gvchganegoda@gmail.com>
2025-09-14 14:45:08 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -11,7 +11,7 @@ cmd(
     filename: __filename,
   },
   async (
-    danuwa,
+    gvbud,
     mek,
     m,
     {
@@ -55,7 +55,7 @@ Song downloader
 🔗 *Watch Here:* ${data.url}
 `;
 
-      await danuwa.sendMessage(
+      await gvbud.sendMessage(
         from,
         { image: { url: data.thumbnail }, caption: desc },
         { quoted: mek }
@@ -74,7 +74,7 @@ Song downloader
         return reply("⏳ *Sorry, audio files longer than 30 minutes are not supported.*");
       }
 
-      await danuwa.sendMessage(
+      await gvbud.sendMessage(
         from,
         {
           audio: { url: songData.download.url },
@@ -83,7 +83,7 @@ Song downloader
         { quoted: mek }
       );
 
-      await danuwa.sendMessage(
+      await gvbud.sendMessage(
         from,
         {
           document: { url: songData.download.url },
@@ -94,7 +94,7 @@ Song downloader
         { quoted: mek }
       );
 
-      return reply("✅ Thank you");
+      return reply("✅ Thank you for using GV-Bud");
     } catch (e) {
       console.log(e);
       reply(`❌ *Error:* ${e.message} 😞`);
