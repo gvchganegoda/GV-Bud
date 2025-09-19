c1b9c9f14c0e32eb44510e56a143de474d2cb720
gvchganegoda <gvchganegoda@gmail.com>
2025-09-20 00:10:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "uR1SQBZC#-u0OrWdQqxJkRquQLM0YjpX-yxH1LQ2TtNUVJZick6w",
+SESSION_ID: process.env.SESSION_ID || "aY8hTDrI#u6sBdoQk5Sjns2ZLC3OfCct2cqMOGm8g0RZ30WuD3bk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/gvchganegoda/GV-Bud/blob/main/images/gv%20bud.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 GV-Bud Is Alive Now😍*",
 BOT_OWNER: '94787114501',  // Replace with the owner's phone number
